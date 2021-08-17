bbe6ebeb5c35a97351c1213f81bf16297c9f914a
blankaf <realblankaf@gmail.com>
2021-08-17 03:24:28 +0000

    sm6375-common: init: Adapt for libperfmgr
    Co-authored-by: Ramii Ahmed <ramy@ahmedramy.com>

--- common.mk
+++ common.mk
@@ -238,6 +238,7 @@ PRODUCT_PACKAGES += \
     init.blair.post_boot.sh \
     init.oplus.rc \
     init.qcom.early_boot.sh \
+    init.qcom.power.rc \
     init.qcom.rc \
     init.qcom.recovery.rc \
     init.qcom.sh \
