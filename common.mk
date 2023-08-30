7342f29373594ec00deaca58006f5c6c0f2c487c
Sugakesshaa <sugakesshaa@gmail.com>
2023-08-30 23:57:13 +0800

    sm6375-common: Don't build udfps dependencies
    Change-Id: I3979e0e14d5daf9d69972e25b5b991c99f0cd821

--- common.mk
+++ common.mk
@@ -334,11 +334,7 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@2.0-service.multihal \
-    libsensorndkbridge \
-    sensors.oplus
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
+    libsensorndkbridge
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
